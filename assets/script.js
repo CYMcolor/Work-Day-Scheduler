aab1a91e3b96b9602952d663229cd68d5ed888ba
Camille MacLaren <camillemaclaren@gmail.com>
2023-06-17 00:48:13 -0500

    update the thing by the hour
    button is clickable

--- assets/script.js
+++ assets/script.js
@@ -3,36 +3,40 @@
 // in the html.
 var today = dayjs();
 var hour = today.$H;
+var currHour;
 console.log(today);
 console.log(hour);
+$('#currentDay').text(today); //imeddiatley shows time upon render
 
 $(function () {
+  // TODO: Add code to display the current date in the header of the page.
+  timeCounter();
+  //create the dom
+  console.log("before the dom hour: " + currHour);
+  domCreate();
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $(".saveBtn").click(function(event)
+  $("button").click(function(event)
   {
-    event.stopPropgation();
-    //event.StopImmediatePropagation();
     console.log("was clicked");
   });
-  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  domCreate();
+  
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  // TODO: Add code to display the current date in the header of the page.
-  timeCounter();
+  
 });
 
 function domCreate()
@@ -69,6 +73,7 @@ function domCreate()
     var grey = '#d3d3d3';
     var red = '#ff6961';
     var green = '#77dd77';
+    console.log("Hour in function: " +hour);
     if(hour === i)
     {
       $("#hour-"+i).addClass("present");
@@ -85,7 +90,6 @@ function domCreate()
       textArea.css({'background-color': green, 'color': 'white'});
     }
       
-    
   }
   //append icon outside or the icon would appear 12 times
   $("button").append(saveIcon);
@@ -97,6 +101,17 @@ function timeCounter()
     {
         today = dayjs();
         $('#currentDay').text(today);
+
+        //if hour changes update dom to reflect hour change
+        currHour = today.$H;
+        console.log("this hour is: "+ currHour);
+        if(hour != currHour)
+        {
+          hour= currHour;
+          $("#schedule").children().remove();
+          domCreate();
+        }
+          
               
     },1000 );
 }
